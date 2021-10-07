5e0ac4c6f906def32aacf07eed32b85b0262508a
leilahsmith <leilah.sdev@gmail.com>
2021-10-07 14:36:01 -0300

    Add project files.

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,43 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+
+        private RestaurantDbContext _db = new RestaurantDbContext();
+        // GET: Restaurant/Index
+        public ActionResult Index()
+        {
+            return View(_db.Restaurants.ToList());
+        }
+
+        // GET: Restaurant/Create (A method to get our view)
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost] //(This says this is ONLY a http post method)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+
+
+    }
+}
